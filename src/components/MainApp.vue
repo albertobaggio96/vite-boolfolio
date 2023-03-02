b1383d103a92a3b947ac170b1286725ab5126829
albertobaggio96 <albertobaggio96@gmail.com>
2023-03-02 15:49:53 +0100

    catch projects api

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,8 +1,40 @@
-<script setup>
+<script>
+  import axios from 'axios';
 
+  export default{
+    name: 'mainApp',
+    data() {
+      return {
+        projectsUrl: 'http://127.0.0.1:8000/api/projects',
+        projects: '',
+      }
+    },
+    methods:{
+      gatProjectsApi(){
+        axios.get(this.projectsUrl, {
+          params: {
+          }
+        })
+          .then((response)=>{
+            console.log(response.data.results.data)
+            this.projects = response.data.results.data
+          })
+          .catch(function (error) {
+          console.log(error);
+          })
+      }
+    },
+    created(){
+      this.gatProjectsApi()
+    }
+  }
 </script>
 
 <template>
+  
+  <div v-for="project in projects">
+    {{ project.title }}
+  </div>
 
 </template>
 
