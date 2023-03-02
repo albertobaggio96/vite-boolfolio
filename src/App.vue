1b4814048dd2043c6e6c29c25d5a989ba99be6b0
albertobaggio96 <albertobaggio96@gmail.com>
2023-03-02 15:32:12 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+  import MainApp from './components/MainApp.vue';
+
+  export default{
+    name:app,
+    components:{
+      MainApp,
+    }
+  }
+</script>
+
+<template>
+  <MainApp/>
+</template>
+
+<style lang="scss">
+  @use './styles/general.scss'as *;
+  @use 'bootstrap/scss/bootstrap.scss' as *;
+
+  
+</style>
