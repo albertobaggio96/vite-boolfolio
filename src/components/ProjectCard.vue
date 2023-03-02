bd5707b7701335886a7d95ffa8a88167de51b980
albertobaggio96 <albertobaggio96@gmail.com>
2023-03-02 18:17:18 +0100

    paginate

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -7,13 +7,12 @@ export default {
 </script>
 
 <template>
-  <article class="card mb-3">
-  <h1 class="card-header">
+  <h1 class="card-header fs-4">
     {{ project.title }}
   </h1>
   <div class="card-body">
-    <img v-if="project.preview.startsWith('img/') " :src="'http://127.0.0.1:8000/storage/' + project.preview " :alt="project.title">
-    <img v-else :src="project.preview " :alt="project.title">
+    <img v-if="project.preview.startsWith('img/') " :src="'http://127.0.0.1:8000/storage/' + project.preview " :alt="project.title" class="img-fluid">
+    <img v-else :src="project.preview " :alt="project.title" class="img-fluid">
     <p class="card-title">Author: {{ project.author }}</p>
     <p class="card-text">Date: {{ project.date }}</p>
     <p class="card-text">Type: {{ project?.type?.type ?? 'unknown' }}</p>
@@ -23,7 +22,6 @@ export default {
     </p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
-</article>
 </template>
 
 <style lang="scss">
