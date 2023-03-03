a10a857c18cca8c517424cf3f594f5885305fcb6
albertobaggio96 <albertobaggio96@gmail.com>
2023-03-03 15:17:35 +0100

    router-link

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <script>
   import HeaderApp from './components/HeaderApp.vue';
-  import MainApp from './components/MainApp.vue';
+  // import MainApp from './components/MainApp.vue';
 
   export default{
     name:app,
     components:{
       HeaderApp,
-      MainApp,
+      // MainApp,
     }
   }
 </script>
 
 <template>
   <HeaderApp/>
-  <MainApp/>
+  <router-view></router-view>
+  <!-- <MainApp/> -->
 </template>
 
 <style lang="scss">
