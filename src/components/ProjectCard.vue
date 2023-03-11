27f917b6bdbbb6f23b40a5d5ab028207cb18e448
albertobaggio96 <albertobaggio96@gmail.com>
2023-03-11 13:44:25 +0100

    fix bug of author name

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,7 +1,7 @@
 <script>
 export default {
   name: 'ProjectCard',
-  props:[
+  props: [
     'project'
   ]
 }
@@ -12,19 +12,17 @@ export default {
     {{ project.title }}
   </h1>
   <div class="card-body">
-    <img v-if="project.preview.startsWith('img/') " :src="'http://127.0.0.1:8000/storage/' + project.preview " :alt="project.title" class="img-fluid">
-    <img v-else :src="project.preview " :alt="project.title" class="img-fluid">
-    <p class="card-title">Author: {{ project.author }}</p>
+    <img v-if="project.preview.startsWith('img/')" :src="'http://127.0.0.1:8000/storage/' + project.preview"
+      :alt="project.title" class="img-fluid">
+    <img v-else :src="project.preview" :alt="project.title" class="img-fluid">
+    <p class="card-title">Author: {{ project.user.name }}</p>
     <p class="card-text">Date: {{ project.date }}</p>
     <p class="card-text">Type: {{ project?.type?.type ?? 'unknown' }}</p>
     <p class="card-text">Technologies: {{ project.slug }}
-      <span v-if="project.technologies.length > 0" v-for="technology in project.technologies">{{ technology.technology + ' '}}</span>
+      <span v-if="project.technologies.length > 0" v-for="technology in project.technologies">{{ technology.technology + ''}}</span>
       <span v-else>unknown</span>
     </p>
-    <router-link :project="'ciao'" :to="{ name: 'project', params: { slug: project.slug }} ">Show</router-link>
   </div>
 </template>
 
-<style lang="scss">
-  
-</style>
+<style lang="scss"></style>
